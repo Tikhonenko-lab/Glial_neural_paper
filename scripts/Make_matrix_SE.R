c1d452412e6d4a2c0abff0ad6c91384843bdeeae
pamelamishra@gmail.com <mishrap@reslnvhpc0386.research.chop.edu>
2024-09-27 15:45:04 -0400

    New files

--- scripts/Make_matrix_SE.R
+++ scripts/Make_matrix_SE.R
@@ -6,3 +6,22 @@ library(viridis)
 library(purrr)
 library(ggrepel)
 
+
+#Find 20% of the samples containing microexons
+#Input : Dataframe for NBL/HGG/DMG etc.
+find_percent_samples <- function(df){
+  uniq<- as.numeric(length(unique(df$Kids_First_Biospecimen_ID)))
+  df<- df %>% mutate(Percent_of_samples = (Num_of_samples/uniq) *100  )
+  return(df)
+
+}
+
+
+#add size of the microexon
+#Input: Dataframe for NBL/HGG/DMG etc 
+add_size_microexon<- function(df_micro){
+
+  df_micro <-df_micro %>% mutate(Size_micro = exonEnd - exonStart_0base)
+  return (df_micro)
+}
+
