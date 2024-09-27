d677890ecc8d7d8eea911e591696fb43eb2a47e4
pamelamishra@gmail.com <mishrap@reslnvhpc0386.research.chop.edu>
2024-09-27 18:34:20 -0400

    New changes

--- scripts/Modules_make_matrix.R
+++ scripts/Modules_make_matrix.R
@@ -48,13 +48,14 @@ return_filtered_df <-function (df){
 find_common_elements<- function(list1, df){
   new_df<- df %>% filter(Microexon %in% list1)
   return (new_df)
-
+}
 
 #Function to read the data from OpenPedCan and extract only SE 
 #Extract only RNA-Seq samples and non-NA cancer types
-read_only_SE <- function(file){
-    #PLEASE MAKE SURE THAT THIS IS SE
-      SE<- fread("../data/OpenPedCan-analysis/data/v13/splice-events-rmats.tsv")
+read_only_SE <- function(){
+
+      SE<- fread("../data/OpenPedCan-analysis/data/v13/histologies.tsv")
+      
       #make a new df with a reduced number of columns
       cols<- c("splicing_case", "geneSymbol", "sample_id", "chr", "strand", "exonStart_0base", "exonEnd"  , "upstreamES",
          "upstreamEE",  "downstreamES", "downstreamEE", "IJC_SAMPLE_1", "SJC_SAMPLE_1","IncLevel1")
@@ -64,18 +65,11 @@ read_only_SE <- function(file){
       #Take histology data in
       hist <- fread("../data/OpenPedCan-analysis/data/v13/histologies.tsv")
       SE_trunc <- SE_trunc %>% filter(splicing_case == "SE")
-      #dim(SE_trunc)
-      #195516372        14
 
       hist<- hist %>% filter(experimental_strategy =="RNA-Seq")
       #hist<- hist %>% filter(cancer_group !="NA")
 
-      # dim(hist)
-      #[1] 31918    58
       SE_trunc_hist<- left_join(SE_trunc, hist, by= "Kids_First_Biospecimen_ID" )
-      #dim(SE_trunc_hist)
-      #[1] 195516372        71
-   
 
       SE_trunc_final <- SE_trunc_hist %>% select(all_of(cols), cancer_group)
       #Then check for NAs
@@ -87,8 +81,6 @@ read_only_SE <- function(file){
       saveRDS(SE_trunc_final, file="SE_noNA_data.RData")
 
       return (SE_trunc_final)
-
-
 }
 
 #Return a pivoted df after selecting a few columsn
---
+++ scripts/get_Matrix.R
@@ -0,0 +1,6 @@
+library(data.table)
+library(dplyr)
+
+source ("Modules_make_matrix.R")
+SE_matrix <- read_only_SE()
+head(SE_matrix)
