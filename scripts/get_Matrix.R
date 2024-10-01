ad5803a1e58c156ec8cb3e0e1232ad66ba9263e1
pamelamishra@gmail.com <mishrap@reslnvhpc0386.research.chop.edu>
2024-10-01 12:49:59 -0400

    Add new modules

--- scripts/get_Matrix.R
+++ scripts/get_Matrix.R
@@ -27,3 +27,15 @@ NBL <- make_cancer_matrix(SE_matrix, "NBL")
 NBL <- run_all(NBL)
 #Select 90% of NBL samples have the microexon_full.
 NBL_90 <- NBL[NBL$Percent_of_samples >=90,]
+
+#fOR THE HEATMAP
+h1<-NBL_90 %>% select(Kids_First_Biospecimen_ID, IncLevel1, Microexon_full)
+h2<- return_pivot(h1)
+h2<- h2 %>%  tibble::column_to_rownames("Microexon_full")
+h2<-as.matrix(h2)
+#if samples have more NA's then remove those NAs (diffrent module)
+
+pheatmap(h2, color= viridis(50),show_colnames = FALSE, 
+         na_col = "grey" ,fontsize_row = 2.1,
+          treeheight_col = 0 , fontsize=8, 
+         main= "NBL microexons <=30,div by 3,90% samples have row microexon")
